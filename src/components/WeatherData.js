71766259cb8fc5b46b2a64108bb8543aaa2604dd
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-11 22:10:29 -0600

    2 new components child of WeatherData

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import WeatherTemperature from './WeatherTemperature.js';
+import WeatherExtraInfo from './WeatherExtraInfo.js';
 
 const WeatherData = () => {
   return(
     <div>
       <p>WeatherData</p>
+      <WeatherTemperature />
+      <WeatherExtraInfo />
     </div>
   );
 }
