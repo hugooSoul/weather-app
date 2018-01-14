e6a6d73b3d2daaaa374a2c37bb686f8c16316786
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-14 14:19:31 -0600

    adding validation to the rest of files

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Location = (props) => {
+const Location = ({city}) => {
   return(
     <div>
-      <h1>{props.city}</h1>
+      <h1>{city}</h1>
     </div>
   );
 }
+
+Location.propTypes = {
+  city: PropTypes.string.isRequired,
+};
 
 export default Location;
