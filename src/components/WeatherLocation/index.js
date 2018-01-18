6ab76b2005f7401a2d5d49366c16a351c81280c9
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-17 20:15:07 -0600

    onClick event & using State to make the update

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,20 +2,45 @@ import React, { Component } from 'react';
 import Location from './Location.js';
 import WeatherData from './WeatherData';
 import './styles.css';
-import {NIGHT_CLOUDY} from './../../constants/weathers';
+import {NIGHT_CLOUDY, WINDY} from './../../constants/weathers';
 
-const data = {
+const data1 = {
   temperature: 17,
   weatherState: NIGHT_CLOUDY,
   humidity: 41,
   wind: "5 km/h",
 };
 
+const data2 = {
+  temperature: 6,
+  weatherState: WINDY,
+  humidity: 20,
+  wind: "5 km/h",
+};
+
 class WeatherLocation extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      city: "Guadalajara, Jal.",
+      data: data1,
+    };
+  }
+
+  handleUpdateClick = () => {
+    this.setState({
+      city: "Guadalajara, Jal.",
+      data: data2,
+    });
+    console.log("Actualizado");
+  }
+
   render = () => (
     <div className="WeatherLocationContent">
-      <Location city={"Guadalajara, Jal."} />
-      <WeatherData data={data} />
+      <Location city={this.state.city} />
+      <WeatherData data={this.state.data} />
+      <button onClick={this.handleUpdateClick} >Update</button>
     </div>
   );
 }
