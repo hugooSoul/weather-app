725582c2e0581051b29b78cc35b57775396da03c
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-17 19:58:26 -0600

    creating a Class Component and transformin WeatherLocation as Component

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location.js';
 import WeatherData from './WeatherData';
 import './styles.css';
 import {NIGHT_CLOUDY} from './../../constants/weathers';
 
 const data = {
-  temperature: 12,
+  temperature: 17,
   weatherState: NIGHT_CLOUDY,
-  humidity: 10,
-  wind: "10 m/s",
+  humidity: 41,
+  wind: "5 km/h",
 };
 
-const WeatherLocation = () => (
+class WeatherLocation extends Component {
+  render = () => (
     <div className="WeatherLocationContent">
       <Location city={"Guadalajara, Jal."} />
       <WeatherData data={data} />
     </div>
-);
+  );
+}
 
 export default WeatherLocation;
