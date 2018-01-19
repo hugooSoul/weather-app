6b1654508b83efc7c14b9f979c2b6e39f62547a0
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-18 21:21:05 -0600

    Display a Loading text & removing the update button cause it's taking the API info

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,19 +2,11 @@ import React, { Component } from 'react';
 import Location from './Location.js';
 import WeatherData from './WeatherData';
 import transformWeather from './../../services/transformWeather';
-import {NIGHT_CLOUDY} from './../../constants/weathers';
 import './styles.css';
 
 const location = "Guadalajara,mx";
 const api_key = "1774ef563dda9e9b0a54091fe568315e";
 const api_weather = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}`;
-
-const data1 = {
-  temperature: 17,
-  weatherState: NIGHT_CLOUDY,
-  humidity: 41,
-  wind: "5 km/h",
-};
 
 class WeatherLocation extends Component {
 
@@ -22,22 +14,18 @@ class WeatherLocation extends Component {
     super();
     this.state = {
       city: "Guadalajara, Jal.",
-      data: data1,
+      data: null,
     };
   }
 
   handleUpdateClick = () => {
-
     fetch(api_weather).then( data => {
-      console.log(data);
       return data.json();
     }).then( weather_data => {
       const data = transformWeather(weather_data);
       this.setState({
         data
       });
-      console.log(weather_data);
-      debugger;
     });
   }
 
@@ -50,11 +38,10 @@ class WeatherLocation extends Component {
     return (
       <div className="WeatherLocationContent">
         <Location city={city} />
-        <WeatherData data={data} />
-        <button onClick={this.handleUpdateClick} >Update</button>
+        {data ? <WeatherData data={data} /> : 'Loading...' }
       </div>
     );
   };
 }
-
+//<button onClick={this.handleUpdateClick} >Update</button>
 export default WeatherLocation;
