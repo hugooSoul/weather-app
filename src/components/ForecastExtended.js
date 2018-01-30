56f28e9a0615e80f3e06de0fa2657a1f86e357da
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-29 21:15:06 -0600

    litthe bit style on ForecastExtended

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './styles.css'
 
 class ForecastExtended extends Component {
   render() {
     const city = this.props.city;
     return(
       <div>
-        <h2>{ city }</h2>
+        <h2 className="forecast-title">{ city }</h2>
       </div>
     );
   }
