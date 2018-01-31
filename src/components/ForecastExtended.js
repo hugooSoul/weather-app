185cc8463cd18a5f81d232b999285df111aad4db
Hugo López Mandujano <hugol.soul@gmail.com>
2018-01-30 21:50:02 -0600

    refactoring ForecastExtended first step

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import ForecastItem from './ForecastItem';
 import './styles.css'
 
-const days = [
+/*const days = [
   "Monday",
   "Tuesday",
   "Wednesday",
@@ -18,20 +18,34 @@ const data = {
   humidity: 10,
   weatherState: "normal",
   wind: "normal",
-};
+};*/
 
 class ForecastExtended extends Component {
 
+  constructor() {
+    super();
+    this.state = { forecastData: null }
+  }
+
   renderForecastItemDays() {
-    return days.map( day => <ForecastItem weekDay={ day } hour={ 10 } data={data} />  );
+    return "Render Items";
+    //return days.map( day => <ForecastItem weekDay={ day } hour={ 10 } data={data} />  );
+  }
+
+  renderProgress()  {
+    return(<h4>Loading data...</h4>);
   }
 
   render() {
     const city = this.props.city;
+    const { forecastData } = this.state;
     return(
       <div>
         <h2 className="forecast-title">{ city }</h2>
-        { this.renderForecastItemDays() }
+        { forecastData ?
+          this.renderForecastItemDays() :
+          this.renderProgress()
+        }
       </div>
     );
   }
