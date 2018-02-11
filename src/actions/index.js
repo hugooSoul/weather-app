bd269c927a2accb7cae51fda053c6617a7ee471f
Hugo López Mandujano <hugol.soul@gmail.com>
2018-02-11 13:34:07 -0600

    react-thunk installed and using applyMiddleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,24 @@
 export const SET_CITY = 'SET_CITY';
 export const setCity = payload => ({ type: SET_CITY, payload });
+
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+const api_key = "1774ef563dda9e9b0a54091fe568315e";
+
+export const fetchForecast = payload => {
+  return distpatch => {
+
+    const url_forecast = `${url}?q=${city}&appid=${api_key}`;
+
+    // active in state a search flag
+    fetch(url_forecast).then(
+      data => ( data.json() )
+    ).then(
+      weather_data => {
+        const forecastData = transformForecast(weather_data);
+
+        //modify state with promise result (fetch)
+      }
+    );
+
+  };
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { city } from './../reducers/city';
 
 const initialState = {
   city: 'Mexico City,MX'
 };
 
-export const store = createStore( city, initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore( city, initialState, composeEnhancers(applyMiddleware(thunk)));
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
