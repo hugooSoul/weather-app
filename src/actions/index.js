4a8ab63025ed712a930eab14ce3ce3517bbf5d8c
Hugo López Mandujano <hugol.soul@gmail.com>
2018-02-11 14:09:26 -0600

    react-materialize saved, new setSelectedCity action created

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,30 @@
+import transformForecast from './../services/transformForecast';
+
 export const SET_CITY = 'SET_CITY';
-export const setCity = payload => ({ type: SET_CITY, payload });
+export const SET_FORECAST_DATA = 'SET_FORECAST_DATA';
+
+const setCity = payload => ({ type: SET_CITY, payload });
+const setForecastData = payload => ({ type: SET_FORECAST_DATA, payload});
 
 const url = "http://api.openweathermap.org/data/2.5/forecast";
 const api_key = "1774ef563dda9e9b0a54091fe568315e";
 
-export const fetchForecast = payload => {
+export const setSelectedCity = payload => {
   return distpatch => {
 
-    const url_forecast = `${url}?q=${city}&appid=${api_key}`;
+    const url_forecast = `${url}?q=${payload}&appid=${api_key}`;
 
     // active in state a search flag
-    fetch(url_forecast).then(
+    distpatch(setCity(payload));
+
+    return fetch(url_forecast).then(
       data => ( data.json() )
     ).then(
       weather_data => {
         const forecastData = transformForecast(weather_data);
 
-        //modify state with promise result (fetch)
+        // modify state with promise result (fetch)
+        distpatch(setForecastData({city: payload, forecastData}));
       }
     );
 
